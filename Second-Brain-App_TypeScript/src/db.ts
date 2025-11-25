11d4ded843f7ef3cff9aef61a0f2d7bb0a486ac2
Gyana Ranjan <gyanaranjansahoo174@gmail.com>
2025-11-25 23:45:16 +0530

    Added some endpoints

--- Second-Brain-App_TypeScript/src/db.ts
+++ Second-Brain-App_TypeScript/src/db.ts
@@ -1,1 +1,18 @@
 //create the user models and schemas here
+import mongoose, {model, Model, Schema} from "mongoose";
+
+const UserSchema = new Schema({
+    username : {type : String, unique : true},
+    password : String,
+})
+
+const ContentSchema = new Schema({
+    title : String,
+    link : String,
+    tags : [{type : mongoose.Types.ObjectId, ref : 'Tag'}],
+    userId : {type : mongoose.Types.ObjectId, ref : 'User', require : true}
+})
+
+export const UserModel = model("User", UserSchema);
+export const ContentModel = model("Content", ContentSchema);
+
--- Second-Brain-App_TypeScript/src/index.ts
+++ Second-Brain-App_TypeScript/src/index.ts
@@ -1,33 +1,110 @@
 import express from "express";
 import mongoose from "mongoose";
 import jwt from "jsonwebtoken";
+import { ContentModel, UserModel } from "./db.js";
+import { JWT_PASSWORD } from "./config.js";
+import { userMiddleware } from "./middleware.js";
+mongoose.connect("mongodb+srv://gyanaranjansahoo174:3pdv1JZAJlt5V2qW@cluster0.cwufgad.mongodb.net/second-brain")
 const app = express();
+app.use(express.json());
 
-app.post("api/v1/signup", (req, res) => {
+app.post("/api/v1/signup", async (req, res) => {
+    //Todo : zod validation and hash the password
+    // And send a 411 status code if user already exists
+    const username = req.body.username;
+    const password = req.body.password;
+
+    try{
+        await UserModel.create({
+        username: username,
+        password : password
+        })
+    
+      res.json({
+        msg : "User signed up"
+      })
+    }catch(e){
+        res.status(411).json({
+            msg : "User already exists"
+        })
+    }
+})
+
+app.post("/api/v1/signin", async (req,res) => {
+    const username = req.body.username;
+    const password = req.body.password;
+
+    const existingUser = await UserModel.findOne({
+        username,
+        password
+    })
+
+    if(existingUser){
+        const token = jwt.sign({
+            id : existingUser._id
+        }, JWT_PASSWORD)
+
+        res.json({
+            token
+        })
+    }else{
+        res.status(403).json({
+            msg : "Incorrect credentials"
+        })
+    }
 
 })
 
-app.post("api/v1/signin", (req,res) => {
+app.post("/api/v1/content", userMiddleware, async (req, res) => {
+    const title = req.body.title;
+    const link = req.body.link;
+
+    await ContentModel.create({
+        title,
+        link,
+        //@ts-ignore
+        userId : req.userId,
+        tags : []
+    })
+    res.json({
+        message : "Content added"
+    })
 
 })
 
-app.post("api/v1/content", (req, res) => {
+app.get("/api/v1/content", userMiddleware, async (req, res) => {
+    //@ts-ignore
+    const userId = req.userId;
+    const content = await ContentModel.find({
+        userId : userId
+    }).populate("userId", "username")
+
+    res.json({
+        content
+    })
+})
+
+app.delete("/api/v1/content", userMiddleware, async (req, res) =>{
+    const contentId = req.body.contentId;
+
+    await ContentModel.deleteMany({
+        contentId,
+        //@ts-ignore
+        userId : req.userId
+    })
+})
+
+app.post("/api/v1/brain/share", (req, res) => {
 
 })
 
-app.get("api/v1/content", (req, res) => {
+app.get("/api/v1/brain/:shareLink", (req, res) => {
 
 })
 
-app.delete("api/v1/content", (req, res) =>{
+function main(){
+    app.listen(3000);
+    console.log(`listening on port 3000`);
+}
 
-})
-
-app.post("api/v1/brain/share", (req, res) => {
-
-})
-
-app.get("api/v1/brain/:shareLink", (req, res) => {
-
-})
-
+main();
